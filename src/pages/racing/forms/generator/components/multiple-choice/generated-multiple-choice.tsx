953fc04c80b81fd90cdd1534848fa1e01ecbd9a3
Jenna Mitchell <99043514+JennaMitchell@users.noreply.github.com>
2024-04-24 14:54:47 -0400

    pixel change

--- src/pages/racing/forms/generator/components/multiple-choice/generated-multiple-choice.tsx
+++ src/pages/racing/forms/generator/components/multiple-choice/generated-multiple-choice.tsx
@@ -79,7 +79,7 @@ const GeneratedMultipleChoice = ({
       const topContainerDivElement = topContainerRefCurrent;
 
       topContainerDivElement.style.transition = `all ${timePerQuestionInSeconds}s ease-in`;
-      topContainerDivElement.style.top = `calc(100vh - 450px)`;
+      topContainerDivElement.style.top = `0px`;
     }
     dispatch(formStoreActions.setStartQuestionTimer(true));
   }, [timePerQuestionInSeconds, dispatch]);
